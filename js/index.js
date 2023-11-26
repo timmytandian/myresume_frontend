66b1c93e6d981798141a28a54307edd31951b556
pop timmytandian <mail@timmytandian.com>
2023-11-26 21:04:15 +0900

    implement getVisitorCount to get data from API GW

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-var visitCounterElmt = document.querySelector("#visit-count");
-var resetButton = document.querySelector("#reset");
+document.addEventListener("DOMContentLoaded", getVisitorCount);
 
-// Adding onClick event listener
-resetButton.addEventListener("click", () => {
-    visitCount = 1;    
-    visitCounterElmt.innerHTML = visitCount;
-});
+async function getVisitorCount(){
+    // select the element to update
+    var visitCounterElmt = document.querySelector("#visit-count");
+
+    // fetch the data from the database
+    const apiEndpoint = new URL("https://3ijz5acnoe.execute-api.ap-northeast-1.amazonaws.com/dev/counts/6632d5b4-5655-4c48-b7b6-071d5823c888?func=addOneVisitorCount");
+    const apiResponse = await fetch(apiEndpoint, {
+        method: "GET",
+    });
+
+    // back to default if connection to API failed
+    if (apiResponse.status === 400) {
+        visitCounterElmt.innerHTML = "(under development)"
+        return
+    }
+    
+    // retrieve the data
+    const responseData = await apiResponse.json();    
+    visitCounterElmt.innerHTML = responseData;
+}
