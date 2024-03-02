70032b049ba1e8505941c8b4e9fa6b27c9fb1dd1
pop timmytandian <mail@timmytandian.com>
2024-03-02 22:04:46 +0900

    update the bug in fetch mock implementation

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,22 +2,28 @@
  * @jest-environment jsdom
  */
 
+import 'isomorphic-fetch'; // fetch API may not be implemented yet in the test environment, so import this
 import { getVisitorCount } from "../js/index.js";
 
-global.fetch = jest.fn(() =>
-    Promise.resolve({
-        json: () => Promise.resolve({ body: 229 })
-    })
-);
+// This is the section where we mock `fetch` API
+const fetchMock = jest
+	.spyOn(global, 'fetch')
+	.mockImplementation(() => 
+        Promise.resolve({ 
+            json: () => Promise.resolve(229),
+            ok: true
+        }));
 
-beforeEach(() => {
-    fetch.mockClear();
-});
-  
 
-it("get visitor count", async () => {
-    const visitorCount = await getVisitorCount();
-
-    expect(visitorCount).toEqual(229);
-    expect(fetch).toHaveBeenCalledTimes(1);
+// This is actual testing suite
+describe('index.js testing', () => {
+	test('getVisitorCount function', async () => {
+		const visitorCount = await getVisitorCount(); // getVisitorCount contains fetch API
+        
+        expect(visitorCount).toEqual(229);
+		expect(fetchMock).toHaveBeenCalledTimes(1);
+        
+        const fetchMockUrl = fetchMock.mock.calls[0][0].href;
+        expect(fetchMockUrl).toBe("https://3ijz5acnoe.execute-api.ap-northeast-1.amazonaws.com/dev/counts/6632d5b4-5655-4c48-b7b6-071d5823c888?func=addOneVisitorCount");
+	});
 });
