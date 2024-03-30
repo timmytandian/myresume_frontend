8581c3a450c85e8d832aeda69eecf7a0cd31f079
pop timmytandian <mail@timmytandian.com>
2024-03-30 16:03:13 +0900

    Made multiple changes: 1) changed the backend folder; 2) upgraded hashicorp/aws provider version; 3) because of depreciation, refactord the website config and acl into separate resources.

--- terraform/s3website/main.tf
+++ terraform/s3website/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-cloudresume-tfstate"
+    bucket         = "terraform-tfstate-cloudresume"
     key            = "frontend/dev_src/terraform.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "terraform-cloudresume-state-locking"
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
     }
   }
 }
@@ -22,40 +22,67 @@ provider "aws" {
 # Recursive upload of multiple files. Reference:
 # https://acode.ninja/posts/recursive-file-upload-to-s3-in-terraform/
 
-resource "aws_s3_bucket" "s3_static" {
+resource "aws_s3_bucket" "tf_resume_timmytandian" {
   bucket  = "tf-resume.timmytandian.com"
-  acl     = "public-read"
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
+  #website {
+  #  index_document = "index.html"
+  #  error_document = "error.html"
+  #}
   # The acl and website keywords below have been depreceated. 
   # See here to troubleshoot: https://discuss.hashicorp.com/t/handling-aws-s3-bucket-acl-deprecation/42288
 }
 
-variable "upload_directory" {
-  default = "${path.cwd}/codebases/website-static/"
+resource "aws_s3_bucket_public_access_block" "tf_resume_timmytandian" {
+  bucket = aws_s3_bucket.tf_resume_timmytandian.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
 }
 
-variable "mime_types" {
-  default = {
-    htm   = "text/html"
-    html  = "text/html"
-    css   = "text/css"
-    ttf   = "font/ttf"
-    js    = "application/javascript"
-    map   = "application/javascript"
-    json  = "application/json"
+resource "aws_s3_bucket_ownership_controls" "tf_resume_timmytandian" {
+  bucket = aws_s3_bucket.tf_resume_timmytandian.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
+  depends_on = [aws_s3_bucket_public_access_block.tf_resume_timmytandian]
 }
 
-resource "aws_s3_bucket_object" "website_files" {
-  for_each      = fileset(var.upload_directory, "**/*.*")
-  bucket        = aws_s3_bucket.s3_static.bucket
-  key           = replace(each.value, var.upload_directory, "")
-  source        = "${var.upload_directory}${each.value}"
+resource "aws_s3_bucket_acl" "tf_resume_timmytandian" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.tf_resume_timmytandian,
+    aws_s3_bucket_public_access_block.tf_resume_timmytandian,
+  ]
+
+  bucket = aws_s3_bucket.tf_resume_timmytandian.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "tf_resume_timmytandian" {
+  bucket = aws_s3_bucket.tf_resume_timmytandian.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  #routing_rule {
+  #  condition {
+  #    key_prefix_equals = "docs/"
+  #  }
+  #  redirect {
+  #    replace_key_prefix_with = "documents/"
+  #  }
+  #}
+}
+
+resource "aws_s3_object" "website_files" {
+  for_each      = fileset(local.upload_directory.default, "**/*.*")
+  bucket        = aws_s3_bucket.tf_resume_timmytandian.bucket
+  key           = replace(each.value, local.upload_directory.default, "")
+  source        = "${local.upload_directory.default}${each.value}"
   acl           = "public-read"
-  etag          = filemd5("${var.upload_directory}${each.value}")
-  content_type  = lookup(var.mime_types, split(".", each.value)[length(split(".", each.value)) - 1])
+  etag          = filemd5("${local.upload_directory.default}${each.value}")
+  content_type  = lookup(local.mime_types.default, split(".", each.value)[length(split(".", each.value)) - 1])
 }
