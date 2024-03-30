95c21f46e4c7058452f9459233abe18e07810fa4
pop timmytandian <mail@timmytandian.com>
2024-03-30 15:59:21 +0900

    recreate the backend state file. Changed the folder to store the state file.

--- terraform/aws-backend/main.tf
+++ terraform/aws-backend/main.tf
@@ -5,7 +5,7 @@ terraform {
   ## TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
   #############################################################
   backend "s3" {
-    bucket         = "terraform-cloudresume-tfstate" # REPLACE WITH YOUR BUCKET NAME
+    bucket         = "terraform-tfstate-cloudresume" # REPLACE WITH YOUR BUCKET NAME
     key            = "import-bootstrap/terraform.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "terraform-cloudresume-state-locking"
@@ -25,7 +25,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "terraform-cloudresume-tfstate" # REPLACE WITH YOUR BUCKET NAME
+  bucket        = "terraform-tfstate-cloudresume" # REPLACE WITH YOUR BUCKET NAME
   force_destroy = true
 }
 
