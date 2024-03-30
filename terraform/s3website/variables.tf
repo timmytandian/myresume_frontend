2e3830e00e38041d8ddc0e8b4f4814abde98d7c3
pop timmytandian <mail@timmytandian.com>
2024-03-30 16:00:29 +0900

    refactor the upload_directory and mime_types terraform variables into local variables

--- terraform/s3website/variables.tf
+++ terraform/s3website/variables.tf
@@ -5,3 +5,27 @@ variable "region" {
   type        = string
   default     = "ap-northeast-1"
 }
+
+locals {
+  upload_directory = {
+    description = "The directory where its content will be uploaded to S3 bucket"
+    default = "${path.cwd}/../../src/"
+  }
+
+  mime_types = {
+    description = "Possibilities of a files mime types"
+    default = {
+      htm   = "text/html"
+      html  = "text/html"
+      css   = "text/css"
+      ttf   = "font/ttf"
+      js    = "application/javascript"
+      map   = "application/javascript"
+      json  = "application/json"
+      jpg   = "image/jpeg"
+      jpeg  = "image/jpeg"
+      png   = "image/png"
+      ico   = "image/vnd.microsoft.icon"
+    }
+  }
+}
