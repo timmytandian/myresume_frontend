7d5aa7d63ce8dc4ad70b0b0485641c614680bedf
pop timmytandian <mail@timmytandian.com>
2024-04-08 22:09:32 +0900

    added the www subdomain bucket name in the output

--- terraform/modules/static_web/outputs.tf
+++ terraform/modules/static_web/outputs.tf
@@ -2,3 +2,8 @@ output "main_static_website_name" {
   value       = aws_s3_bucket.main_static_website.bucket
   description = "The s3 bucket name that stores the static website resource."
 }
+
+output "www_subdomain_static_website_name" {
+  value       = aws_s3_bucket.subdomain_www_static_website.bucket
+  description = "The s3 bucket with www in the bucket name and redirects all request to the main static website. This bucket doesn't store any website resource."
+}
