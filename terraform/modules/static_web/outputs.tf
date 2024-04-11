62da336337b1ff2a49eb9c967dcb05b92fbaf96e
pop timmytandian <mail@timmytandian.com>
2024-04-11 23:41:50 +0900

    added "bucket_regional_domain_name" as output so this bucket can be linked with the cdn module

--- terraform/modules/static_web/outputs.tf
+++ terraform/modules/static_web/outputs.tf
@@ -3,6 +3,11 @@ output "main_static_website_name" {
   description = "The s3 bucket name that stores the static website resource."
 }
 
+output "main_static_website_regional_domain_name" {
+  value       = aws_s3_bucket.main_static_website.bucket_regional_domain_name
+  description = "The bucket region-specific domain name. The bucket domain name including the region name."
+}
+
 output "www_subdomain_static_website_name" {
   value       = aws_s3_bucket.subdomain_www_static_website.bucket
   description = "The s3 bucket with www in the bucket name and redirects all request to the main static website. This bucket doesn't store any website resource."
