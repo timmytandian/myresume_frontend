95273f05fcfea4e1faa3cac71fa5e29d8d6db57d
pop timmytandian <mail@timmytandian.com>
2024-04-13 13:54:17 +0900

    added comment for the distribution

--- terraform/modules/cdn/main.tf
+++ terraform/modules/cdn/main.tf
@@ -18,6 +18,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     viewer_protocol_policy = "redirect-to-https"
   }
 
+  comment = "Simple CDN to distribute my cloud resume in ${var.env} environment. Created by Terraform."
   price_class = "PriceClass_100"
   enabled             = true
   is_ipv6_enabled     = false
