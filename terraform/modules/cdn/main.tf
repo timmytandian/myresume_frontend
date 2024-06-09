bf437d61d8dfbb4ac1853ec7547a40da91a0062e
pop timmytandian <mail@timmytandian.com>
2024-06-09 19:52:06 +0900

    feat: 1) specify cloudfront cache policy for the distribution. 2) define the cloudfront logging. 3) specify the required AWS region/provider for the distribution.

--- terraform/modules/cdn/main.tf
+++ terraform/modules/cdn/main.tf
@@ -15,16 +15,12 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "origin-${var.website_bucket_name_main}"
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "origin-${var.website_bucket_name_main}"
     viewer_protocol_policy = "redirect-to-https"
+    cache_policy_id        = data.aws_cloudfront_cache_policy.s3_distribution.id
+    compress               = true 
     lambda_function_association {
       event_type   = "origin-response"
       lambda_arn   = aws_lambda_function.lambda_edge.qualified_arn
@@ -32,12 +28,14 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
   }
 
-  aliases = [var.website_bucket_name_main, var.website_bucket_name_www]
-  comment = "My cloud resume CDN for ${var.env} environment. Managed from Terraform."
-  price_class = "PriceClass_100"
+  aliases             = [var.website_bucket_name_main, var.website_bucket_name_www]
+  comment             = "My cloud resume CDN for ${var.env} environment. Managed from Terraform."
+  http_version        = "http2and3"
+  is_ipv6_enabled     = true
+  price_class         = "PriceClass_100"
   enabled             = true
-  is_ipv6_enabled     = false
   default_root_object = "index.html"
+  provider            = aws.use1
 
   viewer_certificate {
     cloudfront_default_certificate = false
@@ -46,9 +44,26 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     ssl_support_method             = "sni-only"
   }
 
+  logging_config {
+    bucket          = data.aws_s3_bucket.s3_distribution_logs.id
+    prefix          = "cdn_logs/"
+    include_cookies = false
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
   }
+}
+
+data "aws_cloudfront_cache_policy" "s3_distribution" {
+  name = "Managed-CachingOptimized"
+  provider = aws.use1
+}
+
+# for logging purpose
+data "aws_s3_bucket" "s3_distribution_logs" {
+  bucket = "logs.timmytandian.com"
+  provider = aws.apne1
 }
