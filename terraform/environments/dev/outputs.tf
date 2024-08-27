bddddfff4bffd9650fe5d196c765a2d9f46e39a8
pop timmytandian <m37656192+timmytandian@users.noreply.github.com>
2024-08-27 23:15:35 +0900

    implement the output for dns module, also change the naming for the output of the static web module

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,7 +1,11 @@
-output "main_static_website_name" {
+output "static_website_name_main" {
   value = module.static_web.main_static_website_name
 }
 
-output "www_subdomain_static_website_name" {
+output "static_website_name_www_subdomain" {
   value = module.static_web.www_subdomain_static_website_name
+}
+
+output "ssl_certificate_arn" {
+  value = module.dns.certificate_arn
 }
