bd11f57ff65135fb007da753b4441da80603d6d9
pop timmytandian <m37656192+timmytandian@users.noreply.github.com>
2025-01-08 18:06:49 +0900

    fix: replace the API Gateway endpoint defined in 'content-security-policy' header in CloudFront origin response.

--- terraform/environments/dev/lambda_edge/index.js
+++ terraform/environments/dev/lambda_edge/index.js
@@ -7,7 +7,7 @@ exports.handler = (event, context, callback) => {
 
     //Set new headers
     headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age= 63072000; includeSubdomains; preload'}];
-    headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'self'; img-src 'self' data: w3.org; script-src 'self' https://cdn.jsdelivr.net/ https://use.fontawesome.com/; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/ https://use.fontawesome.com/; object-src 'none'; font-src fonts.gstatic.com; connect-src https://3ijz5acnoe.execute-api.ap-northeast-1.amazonaws.com/"}];
+    headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'self'; img-src 'self' data: w3.org; script-src 'self' https://cdn.jsdelivr.net/ https://use.fontawesome.com/; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/ https://use.fontawesome.com/; object-src 'none'; font-src fonts.gstatic.com; connect-src https://vo422t3t39.execute-api.ap-northeast-1.amazonaws.com/"}];
     headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
     headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}];
     headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block'}];
